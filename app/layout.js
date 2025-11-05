1c7bd273c4fa28e44dd5ed56aa243643ff3fc9ee
muzammil206 <ismailmuzammil206@gmail.com>
2025-11-05 08:55:23 +0100

    fixed

--- app/layout.js
+++ app/layout.js
@@ -17,16 +17,43 @@ export const metadata = {
     "Geospatial Analysis",
     "West Africa Flood",
   ],
-  authors: [{ name: "ismail muzammil" }],
-  creator: "ismail muzammil",
-  publisher: "ismail muzammil",
+  authors: [{ name: "Ismail Muzammil" }],
+  creator: "Ismail Muzammil",
+  publisher: "Ismail Muzammil",
   themeColor: "#4ade80",
   manifest: "/manifest.json",
-  icons: {
+   icons: {
     icon: "/appsn2.ico",
     pyramid: "/appsn2.ico",
   },
+
+  // 👇 Add these
+  openGraph: {
+    title: "MIFMASS Flood Database",
+    description:
+      "Regional flood database for West Africa - Monitor, record, and manage flood events",
+    url: "https://gmes-mifmas.com/", // <-- replace with your real URL
+    siteName: "MIFMASS Flood Database",
+    images: [
+      {
+        url: "/bg.png", // <-- use your own image in /public folder
+        width: 1200,
+        height: 630,
+        alt: "MIFMASS Flood Database Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "MIFMASS Flood Database",
+    description:
+      "Regional flood database for West Africa - Monitor, record, and manage flood events",
+    images: ["/bg.png"], // same as above
+  },
 };
+
 
 export default function RootLayout({ children }) {
   return (
